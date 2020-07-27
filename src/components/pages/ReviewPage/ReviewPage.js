54a1dd50a2c49fb0a554d959652e3f62071bfc55
Myron <dubtwee@gmail.com>
2020-07-27 09:57:24 -0500

    adding events to navigate to thank you and back to the beginning

--- src/components/pages/ReviewPage/ReviewPage.js
+++ src/components/pages/ReviewPage/ReviewPage.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
 
 class ReviewPage extends Component {
+  handleClickSubmit = () => {
+    this.props.history.push('/thank-you');
+  }
+
   render() {
     return (
       <div>
         <h2>Review?</h2>
+        <button onClick={this.handleClickSubmit}>Submit</button>
       </div>
     );
   }
--- src/components/pages/ThankYouPage/ThankYouPage.js
+++ src/components/pages/ThankYouPage/ThankYouPage.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
 
 class ThankYouPage extends Component {
+  handleClickStartOver = () => {
+    this.props.history.push('/');
+  }
+
   render() {
     return (
       <div>
         <h2>Thank You</h2>
+        <button onClick={this.handleClickStartOver}>Leave New Feedback</button>
       </div>
     );
   }
