1f9b08d5cb3a7e23a6f913e33c07a81c6d499ec0
Myron <dubtwee@gmail.com>
2020-07-27 10:49:13 -0500

    adding form field for feeling step

--- src/components/pages/FeelingPage/FeelingPage.js
+++ src/components/pages/FeelingPage/FeelingPage.js
@@ -2,18 +2,35 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 class FeelingPage extends Component {
+  state = {
+    feelingEntered: '',
+  }
+
   handleClickNext = () => {
     this.props.history.push('/understanding');
     this.props.dispatch({
       type: 'ADD_FEELING',
-      payload: 2
+      payload: parseInt(this.state.feelingEntered),
+    });
+  }
+
+  handleChangeField = (event) => {
+    this.setState({
+      feelingEntered: event.target.value
     });
   }
 
   render() {
     return (
       <div>
-        <h2>Feelings?</h2>
+        <h2>How are you feeling today?</h2>
+        <label htmlFor="feelings">Feeling?</label>
+        <input
+          id="feelings"
+          type="number"
+          placeholder="Number between 1 and 5"
+          onChange={this.handleChangeField}
+        />
         <button onClick={this.handleClickNext}>Next</button>
       </div>
     );
