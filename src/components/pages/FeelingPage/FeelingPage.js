1b76740c52dffd07c10ff8d1e349e41e63364edf
Myron <dubtwee@gmail.com>
2020-07-27 09:51:26 -0500

    navigating from feeling to understaing

--- src/components/pages/FeelingPage/FeelingPage.js
+++ src/components/pages/FeelingPage/FeelingPage.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
 
 class FeelingPage extends Component {
+  handleClickNext = () => {
+    this.props.history.push('/understanding');
+  }
+
   render() {
     return (
       <div>
         <h2>Feelings?</h2>
+        <button onClick={this.handleClickNext}>Next</button>
       </div>
     );
   }
