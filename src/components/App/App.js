047087a0fe30a8136dbefb81ad2ca21688356b38
Myron <dubtwee@gmail.com>
2020-07-27 09:48:03 -0500

    register all page routes

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,6 +3,12 @@ import axios from 'axios';
 import './App.css';
 
 import { HashRouter as Router, Route } from 'react-router-dom';
+import FeelingPage from '../pages/FeelingPage/FeelingPage';
+import UnderstandingPage from '../pages/UnderstandingPage/UnderstandingPage';
+import SupportPage from '../pages/SupportPage/SupportPage';
+import CommentsPage from '../pages/CommentsPage/CommentsPage';
+import ReviewPage from '../pages/ReviewPage/ReviewPage';
+import ThankYouPage from '../pages/ThankYouPage/ThankYouPage';
 
 // TODO - SETUP router for 6 pages
 
@@ -16,6 +22,13 @@ class App extends Component {
             <h4><i>Don't forget it!</i></h4>
           </header>
           <br/>
+          <Route exact path="/" component={FeelingPage} />
+          <Route exact path="/understanding" component={UnderstandingPage} />
+          <Route exact path="/support" component={SupportPage} />
+          <Route exact path="/comments" component={CommentsPage} />
+          <Route exact path="/review" component={ReviewPage} />
+          <Route exact path="/thank-you" component={ThankYouPage} />
+
         </div>
       </Router>
     );
